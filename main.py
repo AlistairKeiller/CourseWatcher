db91cda5c0876884b3d949e5cb0cc9e23f892c75
Alistair Keiller <alistair@keiller.net>
2025-05-28 00:46:19 -0700

    test

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ async def fetch_products_from_site(
     new_products: Set[str] = set()
     page = await browser.new_page()
     try:
-        await page.goto(site_config["url"], timeout=30000)
+        await page.goto(site_config["url"], timeout=60000)
         product_cards = await page.query_selector_all(
             site_config["product_card_selector"]
         )
